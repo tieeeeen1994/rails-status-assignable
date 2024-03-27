600b4c6c4eb75287883de17226de4e0b7c678203
Tien <tieeeeen1994@gmail.com>
2024-03-27 15:00:58 +0800

    add requires

--- lib/active_record/associations/builder/association.rb
+++ lib/active_record/associations/builder/association.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative '../../status_assignable/association'
+
 module ActiveRecord::Associations::Builder # rubocop:disable Style/ClassAndModuleChildren
   class Association
     prepend StatusAssignable::Association
--- lib/active_record/base.rb
+++ lib/active_record/base.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require_relative '../status_assignable/active_record'
 module ActiveRecord
   class Base # rubocop:disable Style/Documentation
     singleton_class.prepend StatusAssignable::ActiveRecord
